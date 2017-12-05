5b409d6f515ea500789a0d0384d8368cdb6d980b
jorge pinedo <jpinedom@hotmail.com>
2017-12-04 23:43:56 -0500

    add

--- app/Http/Controllers/Api/ParkController.php
+++ app/Http/Controllers/Api/ParkController.php
@@ -21,7 +21,7 @@ class ParkController extends Controller {
     }
 
     public function getParks() {
-        $data = Parks::select("id","address","latitude","longitude","value",DB::raw("available-coalesce(current,0) as available"));
+        $data = Parks::select("id","address","latitude","longitude","value",DB::raw("available-coalesce(current,0) as available"))->get();
         
         return response()->json(['data' => $data]);
     }
